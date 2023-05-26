82f6d13bb2db17195df6cbaad84f39a9adc747bc
Yesser <benselmayasser1@gmail.com>
2023-05-27 00:27:10 +0200

    Changment de cases avec UPDATE table SET champ = .. WHERE champ ..

--- main.py
+++ main.py
@@ -3,10 +3,18 @@ import sqlite3
 conn = sqlite3.connect('Base.db')
 c = conn.cursor()
 
-#Selection specifique (ID > 1)
-c.execute("SELECT rowid, * FROM personne WHERE Name LIKE 'Personne%'")
-data = c.fetchall()
-print(data)
+#Affectation avant le changment
+c.execute("SELECT rowid, * FROM personne")
+data_1 = c.fetchall()
+
+#Affectation apres changment et affichage
+c.execute("""UPDATE personne SET Name = 'Personne_A'
+WHERE Name = 'Personne_C'""")
+c.execute("SELECT rowid, * FROM personne")
+data_2 = c.fetchall()
+
+print(data_1)
+print(data_2)
 
 conn.commit()
 conn.close()
