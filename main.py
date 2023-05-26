6265687daf4b7b2ad7b89260c5871e99849ef018
Yesser <benselmayasser1@gmail.com>
2023-05-26 22:48:05 +0200

    Insertion de données dans une table

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ import sqlite3
 conn = sqlite3.connect('Base.db')
 c = conn.cursor()
 
-#Creation de table pour y stocker des informations plus tard
-c.execute("""CREATE TABLE Personne (
-    ID int,
-    Name str
-)""")
+#Insertion de données (1, "Yesser") dans Personne
+c.execute("""INSERT INTO Personne VALUES 
+    (1, "Yesser")
+""")
 
 conn.commit()
 conn.close()
