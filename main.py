4bdd2a2acf350228a319530ee4f78ff6db1878fe
Yesser <benselmayasser1@gmail.com>
2023-05-27 00:24:58 +0200

    WHERE .. LIKE

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ conn = sqlite3.connect('Base.db')
 c = conn.cursor()
 
 #Selection specifique (ID > 1)
-c.execute("SELECT rowid, * FROM personne WHERE ID > 1")
+c.execute("SELECT rowid, * FROM personne WHERE Name LIKE 'Personne%'")
 data = c.fetchall()
 print(data)
 
