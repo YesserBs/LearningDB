52d152b9f8938bee81d8405096e5b99799baccfd
Yesser <benselmayasser1@gmail.com>
2023-05-27 00:40:32 +0200

    Suppression d'un element de la table DELETE from table WHERE ..

--- main.py
+++ main.py
@@ -3,16 +3,18 @@ import sqlite3
 conn = sqlite3.connect('Base.db')
 c = conn.cursor()
 
-#Affectation avant le changment
+#Affectation avant suppression
 c.execute("SELECT rowid, * FROM personne")
 data_1 = c.fetchall()
 
-#Affectation apres changment et affichage
-c.execute("""UPDATE personne SET Name = 'Personne_A'
-WHERE Name = 'Personne_C'""")
+#Suppression de la personne dont la rowid = 3
+c.execute("DELETE from personne WHERE rowid = 3")
+
+#Affectation avant suppression
 c.execute("SELECT rowid, * FROM personne")
 data_2 = c.fetchall()
 
+#Affichage
 print(data_1)
 print(data_2)
 
